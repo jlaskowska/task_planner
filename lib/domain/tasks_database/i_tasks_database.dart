0f15760b44502aaebbf026f6af29388f795758e8
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2022-06-18 12:43:52 +0200

    Update ITasksDatabase with addTag and deleteTag

--- lib/domain/tasks_database/i_tasks_database.dart
+++ lib/domain/tasks_database/i_tasks_database.dart
@@ -1,10 +1,12 @@
 import 'package:task_planner/domain/tasks_database/task_entity.dart';
 
 abstract class ITasksDatabase {
-  Future<void> createTask({required String title, required String id});
+  Future<void> createTask({required String title, required String id, String? tag});
   Future<void> deleteTask(String id);
   Future<void> completeTask(String id);
   Future<TaskEntity> getTask(String id);
   Future<List<TaskEntity>> getAllTasks();
   Stream<List<TaskEntity>> watchAllTasks();
+  Future<void> addTag(String label);
+  Future<void> deleteTag(String label);
 }
