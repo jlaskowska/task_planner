b3c3d3b084edd8fe5696449c48ca507779c097b2
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2022-06-18 12:44:03 +0200

    Update database

--- lib/infrastructure/tasks_database/db.dart
+++ lib/infrastructure/tasks_database/db.dart
@@ -6,12 +6,13 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:task_planner/domain/tasks_database/i_tasks_database.dart';
 import 'package:task_planner/domain/tasks_database/task_entity.dart';
+import 'package:task_planner/infrastructure/tasks_database/tables/tags.dart';
 import 'package:task_planner/infrastructure/tasks_database/tables/tasks.dart';
 import 'package:task_planner/infrastructure/tasks_database/tasks_mapper.dart';
 
 part 'db.g.dart';
 
-@DriftDatabase(tables: [Tasks])
+@DriftDatabase(tables: [Tasks, Tags])
 class TasksDatabase extends _$TasksDatabase implements ITasksDatabase {
   TasksDatabase() : super(_openConnection());
 
@@ -28,8 +29,17 @@ class TasksDatabase extends _$TasksDatabase implements ITasksDatabase {
   }
 
   @override
-  Future<void> createTask({required String title, required String id}) async {
-    final task = Task(title: title, id: id, completed: false);
+  Future<void> createTask(
+      {required String title, required String id, String? tag}) async {
+    if (tag != null) {
+      final tagObject = await (select(tags)
+            ..where((tag1) => tag1.label.equals(tag)))
+          .getSingleOrNull();
+      if (tagObject == null) {
+        await addTag(tag);
+      }
+    }
+    final task = Task(title: title, id: id, tag: tag, completed: false);
     into(tasks).insert(task);
   }
 
@@ -49,14 +59,31 @@ class TasksDatabase extends _$TasksDatabase implements ITasksDatabase {
           .map((task) => TasksMapper.infToDom(task));
 
   @override
-  Stream<List<TaskEntity>> watchAllTasks() =>
-      select(tasks).map((task) => TasksMapper.infToDom(task)).watch();
+  Stream<List<TaskEntity>> watchAllTasks() => select(tasks)
+      .join([leftOuterJoin(tags, tags.label.equalsExp(tasks.tag))])
+      .watch()
+      .map((rows) => rows.map((row) {
+            return TasksMapper.infToDom(row.readTable(tasks));
+          }).toList());
+
+  @override
+  Future<void> addTag(String label) async {
+    final tag = Tag(label: label);
+    into(tags).insert(tag);
+  }
+
+  @override
+  Future<void> deleteTag(String label) async {
+    // DODO Update all taks that have tag
+    (delete(tags)..where((tag) => tag.label.equals(label))).go();
+  }
 }
 
 LazyDatabase _openConnection() {
   return LazyDatabase(() async {
     final dbFolder = await getApplicationDocumentsDirectory();
     final file = File(join(dbFolder.path, 'db.sqlite'));
+    print(file.path);
     return NativeDatabase(file);
   });
 }
--- lib/infrastructure/tasks_database/db.g.dart
+++ lib/infrastructure/tasks_database/db.g.dart
@@ -9,14 +9,21 @@ part of 'db.dart';
 // ignore_for_file: unnecessary_brace_in_string_interps, unnecessary_this
 class Task extends DataClass implements Insertable<Task> {
   final String id;
+  final String? tag;
   final String title;
   final bool completed;
-  Task({required this.id, required this.title, required this.completed});
+  Task(
+      {required this.id,
+      this.tag,
+      required this.title,
+      required this.completed});
   factory Task.fromData(Map<String, dynamic> data, {String? prefix}) {
     final effectivePrefix = prefix ?? '';
     return Task(
       id: const StringType()
           .mapFromDatabaseResponse(data['${effectivePrefix}id'])!,
+      tag: const StringType()
+          .mapFromDatabaseResponse(data['${effectivePrefix}tag']),
       title: const StringType()
           .mapFromDatabaseResponse(data['${effectivePrefix}title'])!,
       completed: const BoolType()
@@ -27,6 +34,9 @@ class Task extends DataClass implements Insertable<Task> {
   Map<String, Expression> toColumns(bool nullToAbsent) {
     final map = <String, Expression>{};
     map['id'] = Variable<String>(id);
+    if (!nullToAbsent || tag != null) {
+      map['tag'] = Variable<String?>(tag);
+    }
     map['title'] = Variable<String>(title);
     map['completed'] = Variable<bool>(completed);
     return map;
@@ -35,6 +45,7 @@ class Task extends DataClass implements Insertable<Task> {
   TasksCompanion toCompanion(bool nullToAbsent) {
     return TasksCompanion(
       id: Value(id),
+      tag: tag == null && nullToAbsent ? const Value.absent() : Value(tag),
       title: Value(title),
       completed: Value(completed),
     );
@@ -45,6 +56,7 @@ class Task extends DataClass implements Insertable<Task> {
     serializer ??= driftRuntimeOptions.defaultSerializer;
     return Task(
       id: serializer.fromJson<String>(json['id']),
+      tag: serializer.fromJson<String?>(json['tag']),
       title: serializer.fromJson<String>(json['title']),
       completed: serializer.fromJson<bool>(json['completed']),
     );
@@ -54,13 +66,16 @@ class Task extends DataClass implements Insertable<Task> {
     serializer ??= driftRuntimeOptions.defaultSerializer;
     return <String, dynamic>{
       'id': serializer.toJson<String>(id),
+      'tag': serializer.toJson<String?>(tag),
       'title': serializer.toJson<String>(title),
       'completed': serializer.toJson<bool>(completed),
     };
   }
 
-  Task copyWith({String? id, String? title, bool? completed}) => Task(
+  Task copyWith({String? id, String? tag, String? title, bool? completed}) =>
+      Task(
         id: id ?? this.id,
+        tag: tag ?? this.tag,
         title: title ?? this.title,
         completed: completed ?? this.completed,
       );
@@ -68,6 +83,7 @@ class Task extends DataClass implements Insertable<Task> {
   String toString() {
     return (StringBuffer('Task(')
           ..write('id: $id, ')
+          ..write('tag: $tag, ')
           ..write('title: $title, ')
           ..write('completed: $completed')
           ..write(')'))
@@ -75,47 +91,57 @@ class Task extends DataClass implements Insertable<Task> {
   }
 
   @override
-  int get hashCode => Object.hash(id, title, completed);
+  int get hashCode => Object.hash(id, tag, title, completed);
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
       (other is Task &&
           other.id == this.id &&
+          other.tag == this.tag &&
           other.title == this.title &&
           other.completed == this.completed);
 }
 
 class TasksCompanion extends UpdateCompanion<Task> {
   final Value<String> id;
+  final Value<String?> tag;
   final Value<String> title;
   final Value<bool> completed;
   const TasksCompanion({
     this.id = const Value.absent(),
+    this.tag = const Value.absent(),
     this.title = const Value.absent(),
     this.completed = const Value.absent(),
   });
   TasksCompanion.insert({
     required String id,
+    this.tag = const Value.absent(),
     required String title,
     this.completed = const Value.absent(),
   })  : id = Value(id),
         title = Value(title);
   static Insertable<Task> custom({
     Expression<String>? id,
+    Expression<String?>? tag,
     Expression<String>? title,
     Expression<bool>? completed,
   }) {
     return RawValuesInsertable({
       if (id != null) 'id': id,
+      if (tag != null) 'tag': tag,
       if (title != null) 'title': title,
       if (completed != null) 'completed': completed,
     });
   }
 
   TasksCompanion copyWith(
-      {Value<String>? id, Value<String>? title, Value<bool>? completed}) {
+      {Value<String>? id,
+      Value<String?>? tag,
+      Value<String>? title,
+      Value<bool>? completed}) {
     return TasksCompanion(
       id: id ?? this.id,
+      tag: tag ?? this.tag,
       title: title ?? this.title,
       completed: completed ?? this.completed,
     );
@@ -126,6 +152,9 @@ class TasksCompanion extends UpdateCompanion<Task> {
     final map = <String, Expression>{};
     if (id.present) {
       map['id'] = Variable<String>(id.value);
+    }
+    if (tag.present) {
+      map['tag'] = Variable<String?>(tag.value);
     }
     if (title.present) {
       map['title'] = Variable<String>(title.value);
@@ -140,6 +169,7 @@ class TasksCompanion extends UpdateCompanion<Task> {
   String toString() {
     return (StringBuffer('TasksCompanion(')
           ..write('id: $id, ')
+          ..write('tag: $tag, ')
           ..write('title: $title, ')
           ..write('completed: $completed')
           ..write(')'))
@@ -156,6 +186,13 @@ class $TasksTable extends Tasks with TableInfo<$TasksTable, Task> {
   late final GeneratedColumn<String?> id = GeneratedColumn<String?>(
       'id', aliasedName, false,
       type: const StringType(), requiredDuringInsert: true);
+  final VerificationMeta _tagMeta = const VerificationMeta('tag');
+  @override
+  late final GeneratedColumn<String?> tag = GeneratedColumn<String?>(
+      'tag', aliasedName, true,
+      type: const StringType(),
+      requiredDuringInsert: false,
+      $customConstraints: 'NULL REFERENCES tags(label)');
   final VerificationMeta _titleMeta = const VerificationMeta('title');
   @override
   late final GeneratedColumn<String?> title = GeneratedColumn<String?>(
@@ -173,7 +210,7 @@ class $TasksTable extends Tasks with TableInfo<$TasksTable, Task> {
       defaultConstraints: 'CHECK (completed IN (0, 1))',
       defaultValue: const Constant(false));
   @override
-  List<GeneratedColumn> get $columns => [id, title, completed];
+  List<GeneratedColumn> get $columns => [id, tag, title, completed];
   @override
   String get aliasedName => _alias ?? 'tasks';
   @override
@@ -187,6 +224,10 @@ class $TasksTable extends Tasks with TableInfo<$TasksTable, Task> {
       context.handle(_idMeta, id.isAcceptableOrUnknown(data['id']!, _idMeta));
     } else if (isInserting) {
       context.missing(_idMeta);
+    }
+    if (data.containsKey('tag')) {
+      context.handle(
+          _tagMeta, tag.isAcceptableOrUnknown(data['tag']!, _tagMeta));
     }
     if (data.containsKey('title')) {
       context.handle(
@@ -215,11 +256,154 @@ class $TasksTable extends Tasks with TableInfo<$TasksTable, Task> {
   }
 }
 
+class Tag extends DataClass implements Insertable<Tag> {
+  final String label;
+  Tag({required this.label});
+  factory Tag.fromData(Map<String, dynamic> data, {String? prefix}) {
+    final effectivePrefix = prefix ?? '';
+    return Tag(
+      label: const StringType()
+          .mapFromDatabaseResponse(data['${effectivePrefix}label'])!,
+    );
+  }
+  @override
+  Map<String, Expression> toColumns(bool nullToAbsent) {
+    final map = <String, Expression>{};
+    map['label'] = Variable<String>(label);
+    return map;
+  }
+
+  TagsCompanion toCompanion(bool nullToAbsent) {
+    return TagsCompanion(
+      label: Value(label),
+    );
+  }
+
+  factory Tag.fromJson(Map<String, dynamic> json,
+      {ValueSerializer? serializer}) {
+    serializer ??= driftRuntimeOptions.defaultSerializer;
+    return Tag(
+      label: serializer.fromJson<String>(json['label']),
+    );
+  }
+  @override
+  Map<String, dynamic> toJson({ValueSerializer? serializer}) {
+    serializer ??= driftRuntimeOptions.defaultSerializer;
+    return <String, dynamic>{
+      'label': serializer.toJson<String>(label),
+    };
+  }
+
+  Tag copyWith({String? label}) => Tag(
+        label: label ?? this.label,
+      );
+  @override
+  String toString() {
+    return (StringBuffer('Tag(')
+          ..write('label: $label')
+          ..write(')'))
+        .toString();
+  }
+
+  @override
+  int get hashCode => label.hashCode;
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || (other is Tag && other.label == this.label);
+}
+
+class TagsCompanion extends UpdateCompanion<Tag> {
+  final Value<String> label;
+  const TagsCompanion({
+    this.label = const Value.absent(),
+  });
+  TagsCompanion.insert({
+    required String label,
+  }) : label = Value(label);
+  static Insertable<Tag> custom({
+    Expression<String>? label,
+  }) {
+    return RawValuesInsertable({
+      if (label != null) 'label': label,
+    });
+  }
+
+  TagsCompanion copyWith({Value<String>? label}) {
+    return TagsCompanion(
+      label: label ?? this.label,
+    );
+  }
+
+  @override
+  Map<String, Expression> toColumns(bool nullToAbsent) {
+    final map = <String, Expression>{};
+    if (label.present) {
+      map['label'] = Variable<String>(label.value);
+    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return (StringBuffer('TagsCompanion(')
+          ..write('label: $label')
+          ..write(')'))
+        .toString();
+  }
+}
+
+class $TagsTable extends Tags with TableInfo<$TagsTable, Tag> {
+  final GeneratedDatabase _db;
+  final String? _alias;
+  $TagsTable(this._db, [this._alias]);
+  final VerificationMeta _labelMeta = const VerificationMeta('label');
+  @override
+  late final GeneratedColumn<String?> label = GeneratedColumn<String?>(
+      'label', aliasedName, false,
+      additionalChecks:
+          GeneratedColumn.checkTextLength(minTextLength: 1, maxTextLength: 10),
+      type: const StringType(),
+      requiredDuringInsert: true);
+  @override
+  List<GeneratedColumn> get $columns => [label];
+  @override
+  String get aliasedName => _alias ?? 'tags';
+  @override
+  String get actualTableName => 'tags';
+  @override
+  VerificationContext validateIntegrity(Insertable<Tag> instance,
+      {bool isInserting = false}) {
+    final context = VerificationContext();
+    final data = instance.toColumns(true);
+    if (data.containsKey('label')) {
+      context.handle(
+          _labelMeta, label.isAcceptableOrUnknown(data['label']!, _labelMeta));
+    } else if (isInserting) {
+      context.missing(_labelMeta);
+    }
+    return context;
+  }
+
+  @override
+  Set<GeneratedColumn> get $primaryKey => {label};
+  @override
+  Tag map(Map<String, dynamic> data, {String? tablePrefix}) {
+    return Tag.fromData(data,
+        prefix: tablePrefix != null ? '$tablePrefix.' : null);
+  }
+
+  @override
+  $TagsTable createAlias(String alias) {
+    return $TagsTable(_db, alias);
+  }
+}
+
 abstract class _$TasksDatabase extends GeneratedDatabase {
   _$TasksDatabase(QueryExecutor e) : super(SqlTypeSystem.defaultInstance, e);
   late final $TasksTable tasks = $TasksTable(this);
+  late final $TagsTable tags = $TagsTable(this);
   @override
   Iterable<TableInfo> get allTables => allSchemaEntities.whereType<TableInfo>();
   @override
-  List<DatabaseSchemaEntity> get allSchemaEntities => [tasks];
+  List<DatabaseSchemaEntity> get allSchemaEntities => [tasks, tags];
 }
