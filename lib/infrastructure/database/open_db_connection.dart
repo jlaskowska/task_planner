8145a5dec0a8e55d057bda2dfe9cfd1bb13ed5eb
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2022-07-02 14:27:39 +0200

    Copy initial db to users documents

--- lib/infrastructure/database/open_db_connection.dart
+++ lib/infrastructure/database/open_db_connection.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
+import 'package:flutter/services.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -9,6 +10,10 @@ LazyDatabase openConnection() {
   return LazyDatabase(() async {
     final dbFolder = await getApplicationDocumentsDirectory();
     final file = File(join(dbFolder.path, 'db.sqlite'));
+    if(! await file.exists()){
+      final asset = await rootBundle.load('assets/db/db.sqlite');
+      await file.writeAsBytes(asset.buffer.asUint8List(asset.offsetInBytes, asset.lengthInBytes));
+    }
     return NativeDatabase(file);
   });
 }
