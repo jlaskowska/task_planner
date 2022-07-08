fb958a528a997b762696fa56a4f6ecf5c7aec8d2
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2022-07-08 19:15:37 +0200

    Add toggle task completion functionality

--- lib/presentation/widgets/task_overview/task_overview.dart
+++ lib/presentation/widgets/task_overview/task_overview.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:provider/provider.dart';
 import 'package:routemaster/routemaster.dart';
 import 'package:task_planner/presentation/widgets/task_overview/task_overview_view_model.dart';
 import 'package:task_planner/presentation/widgets/task_tile.dart';
 
-class TasksOverview extends ConsumerWidget {
+class TasksOverview extends StatelessWidget {
   const TasksOverview({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, ref) {
-    TaskOverviewViewModel viewModel = ref.watch(taskOverviewNotifierProvider);
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+        create: (_) => TaskOverviewViewModel()..initialize(),
+        builder: (context, child) {
+          final viewModel = Provider.of<TaskOverviewViewModel>(context);
 
-    return TasksOverviewContent(
-      viewModel: viewModel,
-    );
+          return TasksOverviewContent(
+            viewModel: viewModel,
+          );
+        });
   }
 }
-
-final taskOverviewNotifierProvider =
-    ChangeNotifierProvider.autoDispose<TaskOverviewViewModel>((ref) {
-  return TaskOverviewViewModel()..initialize();
-});
 
 class TasksOverviewContent extends StatelessWidget {
   final TaskOverviewViewModel viewModel;
@@ -51,7 +50,10 @@ class TasksOverviewContent extends StatelessWidget {
               title: task.title,
               isCompleted: task.isCompleted,
               tag: task.tag?.label,
-              onChanged: (value) {},
+              onChanged: (value) => viewModel.toggleCompleteTask(
+                taskId: task.id,
+                value: value,
+              ),
               onTap: () => Routemaster.of(context).push('/task/${task.id}'),
             ),
         ],
