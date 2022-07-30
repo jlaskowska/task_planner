d015360eb6fc2fda89ec4a6eda138ebaac1890f5
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2022-07-30 13:22:15 +0200

    Update script to add initial tag colors to database

--- bin/generate_db.dart
+++ bin/generate_db.dart
@@ -18,5 +18,18 @@ void main() async {
     () => NativeDatabase(dbFile),
   ));
 
-  db.createTask(title: 'Your first task', tag: 'important');
+  const colors = [
+    'ff65c2f9',
+    'fff7b369',
+    'fff9807c',
+    'ff61F3BB',
+    'ff00E7E2',
+    'ffF488C3',
+    'ffA33370',
+  ];
+  for (final color in colors) {
+    db.addTag(color);
+  }
+
+  db.createTask(title: 'Your first task', tag: 'ff61F3BB');
 }
