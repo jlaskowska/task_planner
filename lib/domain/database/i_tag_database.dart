6b349483cb5eecec5a0a896a3376110e9156d119
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2022-07-30 13:23:37 +0200

    Update ITagDatabase to watch all tags

--- lib/domain/database/i_tag_database.dart
+++ lib/domain/database/i_tag_database.dart
@@ -2,7 +2,7 @@
 import 'package:task_planner/domain/database/tag_entity.dart';
 
 abstract class ITagDatabase {
-  Future<void> addTag(String label);
-  Future<void> deleteTag(String label);
-  Future<List<TagEntity>> getAllTags();
+  Future<void> addTag(String color);
+  Future<void> deleteTag(String color);
+  Stream<List<TagEntity>> watchAllTags();
 }
